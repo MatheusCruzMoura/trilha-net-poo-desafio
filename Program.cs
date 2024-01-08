21c1e37e6a06e3083df4f46f28671fd433465562
Matheus Cruz de Moura <matheuscruz.moura27@gmail.com>
2024-01-08 20:13:58 -0300

    instanciando objetos das classes Nokia e Iphone, e testando o metodo InstalarAplicativo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.Clear();
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "91234-5678", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "98765-4321", modelo: "Modelo 2", imei: "222222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
